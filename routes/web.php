ce21cf86a6156ed1cf7e28bf0d891ab1aafe2ec6
Yvann <marivint.yvann@gmail.com>
2020-05-16 15:05:32 +0200

    Test route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/welcome', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/welcome', function () {
+//     // return view('welcome');
+// 	return 'Hello World';
+// });
+
+// Route::get('/welcome', function () {
+//     // return view('welcome');
+// 	return ['test' => 'lol' ];
+// });
+
+Route::get('/test-get',  function () {
+
+	$name = request('name'); // $_GET
+	// return $name;
+
+  // return view('test');
+  return view('test-get',[
+  	'name' => $name,
+  ]);
+
+});
+
+Route::get('/test-post/{post}',  function ($post) {
+
+	// return $post;
+
+	$posts = [
+		'my-first-post' => 'Hello, this is my first blog post',
+		'my-second-post' => 'Now I am getting the hang of this blogging thing',
+	];
+
+	if(!array_key_exists($post, $posts)){
+		abort(404, 'Sorry that post was not found');
+	}
+
+  return view('test-post',[
+  	'post' => $posts[$post],
+  ]);
+
+  // return view('test-post');
 });
